b567baca69fb4f2534c7aed16d9b7e43f761b78b
Miguel Angel <57561772+D3wulf@users.noreply.github.com>
2021-07-17 03:00:48 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,5 @@
 export const environment = {
   production: true,
   url:'https://us-central1-angular-goty-568f8.cloudfunctions.net',
-  firebase: {
-    apiKey: "AIzaSyBUHO0eG1ZiXFbG_xFYq6htz_e9BOPXfm0",
-    authDomain: "angular-goty-568f8.firebaseapp.com",
-    projectId: "angular-goty-568f8",
-    storageBucket: "angular-goty-568f8.appspot.com",
-    messagingSenderId: "733863092215",
-    appId: "1:733863092215:web:df47952d4b532f8e46d8f2",
-    
-  }
+ 
 };
