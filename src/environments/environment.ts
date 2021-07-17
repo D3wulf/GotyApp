36619ea1f93d6c2c78c8442696424b98a3cf8822
Miguel Angel <57561772+D3wulf@users.noreply.github.com>
2021-07-17 03:00:29 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,15 +5,7 @@
 export const environment = {
   production: false,
   url:'http://localhost:5000/angular-goty-568f8/us-central1',
-  firebase: {
-    apiKey: "AIzaSyBUHO0eG1ZiXFbG_xFYq6htz_e9BOPXfm0",
-    authDomain: "angular-goty-568f8.firebaseapp.com",
-    projectId: "angular-goty-568f8",
-    storageBucket: "angular-goty-568f8.appspot.com",
-    messagingSenderId: "733863092215",
-    appId: "1:733863092215:web:df47952d4b532f8e46d8f2",
-    
-  }
+  
 };
 
 /*
